acceecc8cb50181ea9efaedb7205cffcf4f9d62d
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2018-07-11 13:02:47 +0300

    Revert "added public"
    This reverts commit 4bcfaba8f43ee74c0dc954bd55abaaadc14500fe.

--- public/path----386f3f3ad19b3287c8c1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{102:function(t,o){t.exports={data:{site:{siteMetadata:{title:"Marco Antonio Lillo"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----386f3f3ad19b3287c8c1.js.map
