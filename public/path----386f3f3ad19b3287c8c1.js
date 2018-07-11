4bcfaba8f43ee74c0dc954bd55abaaadc14500fe
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2018-07-11 11:57:41 +0300

    added public

---
+++ public/path----386f3f3ad19b3287c8c1.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{102:function(t,o){t.exports={data:{site:{siteMetadata:{title:"Marco Antonio Lillo"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----386f3f3ad19b3287c8c1.js.map
