b8cefb0f9e441afa7e5fb418977fc67b4e9048b5
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2018-09-05 21:06:46 +0300

    Remove public folder

--- public/path----6a8c2be1a45e81b6b72f.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([60335399758886],{132:function(t,a){t.exports={data:{siteMeta:{siteMetadata:{title:"Marco Antonio Lillo"}}},layoutContext:{}}}});
-//# sourceMappingURL=path----6a8c2be1a45e81b6b72f.js.map
