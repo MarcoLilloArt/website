b24bab93f90a111e3cb699793b2e3c2b92de7fe4
marcoartlillo <45294373+marcoartlillo@users.noreply.github.com>
2019-02-02 10:33:04 +0200

    Update index.js

--- src/components/exhibition/index.js
+++ src/components/exhibition/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import { translate } from 'react-i18next'
 
-import style from './exhibition.module.css'
-import LocationMap from './map'
-
 const Exhibition = ({ t }) =>
 
 export default translate('main')(Exhibition)
